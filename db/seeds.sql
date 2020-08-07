60b921f56de28bd0e2fcad07e63d1cb0266223ed
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2020-08-06 22:07:16 -0500

    Add workout for 2020-08-06

--- db/seeds.sql
+++ db/seeds.sql
@@ -217,3 +217,23 @@ INSERT INTO workout (
   0,
   true
 ) ON CONFLICT DO NOTHING;
+
+INSERT INTO workout (
+  id,
+  date,
+  goal,
+  description,
+  sms_to,
+  mail_to,
+  completed,
+  voting_enabled
+) VALUES (
+  12,
+  '2020-08-06',
+  '<u>Goal:</u>&nbsp;<i>General Fitness</i>',
+  '<b>Exercise(s):</b><br/><ul><li>150 Air Squats</li><li>100 Sit-Ups</li><li>50 Push-Ups</li><li>100 Jumping Jacks</li><li>50 High Knees</li><li>50 Butt-Kicks</li><li>Run 3 Miles</li></ul>',
+  '+1-617-455-7595',
+  'JonathanZaleski@gmail.com',
+  0,
+  true
+) ON CONFLICT DO NOTHING;
